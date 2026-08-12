6dee50a735f5407e3e6c6ddc71edc4929dad565f
kniz <kniz@byeol.io>
2026-08-12 19:28:01 +0900

    fix(macro): remove `BY_VISIT` macro

--- module/meta/common/macro.hpp
+++ module/meta/common/macro.hpp
@@ -61,20 +61,3 @@ private:
     __BY__DECL_ADT_3(ME, SUPER, SUPERTYPE)       \
     __BY__DECL_CLONE(ME)
 #define __BY__DECL_CLASS(...) BY_OVERLOAD(__BY__DECL_CLASS, __VA_ARGS__)
-
-// ACCEPT:
-//      accept the visitor and let it can iterate sub elements.
-//      please check 'visitor' class for more info.
-#define __BY__DECL_VISIT_0() __BY__DECL_VISIT_1(super)
-#define __BY__DECL_VISIT_1(SUPER)                         \
-public:                                                   \
-    using SUPER::accept;                                  \
-    void accept(const visitInfo& i, visitor& v) override; \
-                                                          \
-private:
-#define __BY__DECL_VISIT(...) BY_OVERLOAD(__BY__DECL_VISIT, __VA_ARGS__)
-
-#define __BY__DECL_DEF_VISIT_0() __BY__DECL_DEF_VISIT_1(me)
-#define __BY__DECL_DEF_VISIT_1(ME) \
-    void ME::accept(const visitInfo& i, visitor& v) { v.visit(i, *this); }
-#define __BY__DECL_DEF_VISIT(...) BY_OVERLOAD(__BY__DECL_DEF_VISIT, __VA_ARGS__)
